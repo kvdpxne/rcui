45a3de0174d6c21cee2b3b234088e0473f9fc601
kvdpxne <kvdpxne@gmail.com>
2024-02-25 01:11:22 +0100

    📝 Added comments to clarify the purpose of InstatiationError class.

--- src/main/java/me/kvdpxne/forty2/InstatiationError.java
+++ src/main/java/me/kvdpxne/forty2/InstatiationError.java
@@ -1,7 +1,18 @@
 package me.kvdpxne.forty2;
 
-public final class InstatiationError extends Error {
+/**
+ * Error thrown when instantiation of a specific class is not allowed.
+ */
+public final class InstatiationError
+  extends Error {
 
+  /**
+   * Constructs an instance of {@code InstatiationError} with a message
+   * indicating that instantiation of the specified class is not allowed.
+   *
+   * @param clazz The Class object representing the class that cannot be
+   *              instantiated.
+   */
   InstatiationError(final Class<?> clazz) {
     super(String.format(
       "Instantiation of %s class is not allowed.",
