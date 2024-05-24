6761192f9e987f66cdabed8d2d7e387834137029
kvdpxne <kvdpxne@gmail.com>
2024-05-24 23:14:15 +0200

    🌱 First commit in the new branch (v0.2.0): Initial setup and branch creation.
    - Modified approach for creating unique identifiers.
    - Downgraded Java version from 21 to 8.

---
+++ src/main/java/me/kvdpxne/forty2/Ascii36.java
@@ -0,0 +1,25 @@
+package me.kvdpxne.forty2;
+
+/**
+ *
+ */
+public final class Ascii36 {
+
+  /**
+   *
+   */
+  public static final char[] LOWER_ASCII_36 = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+  };
+
+  /**
+   *
+   */
+  public static final char[] UPPER_ASCII_36 = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+  };
+}
